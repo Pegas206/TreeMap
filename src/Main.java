4c43c119460100286b612e3d4cda5bf2ac2321ac
Alexander Belousov <pegas206@mail.ru>
2022-12-22 16:11:30 +0700

    Задача 1 (обязательная)

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
+import ru.netology.people.Person;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        //Генерация пользователей
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Петр", "Пупкин", 3));
+        people.add(new Person("Сеня", "Третьяк", 5));
+        people.add(new Person("Таня", "Бобрик", 1));
+        people.add(new Person("Андрей", "Дак", 10));
+        people.add(new Person("Анна", "Цин", 6));
+        System.out.println("Исходный список");
+        System.out.println(people);
+        System.out.println("============================================================");
+        System.out.println("Список после сортировки по знатности");
+        Collections.sort(people);
+        System.out.println(people);
+
+    }
 }
--- src/ru/netology/people/Person.java
+++ src/ru/netology/people/Person.java
@@ -1,4 +1,58 @@
 package ru.netology.people;
 
-public class Person {
+import java.util.concurrent.Callable;
+
+public class Person implements Comparable<Person> {
+    private String name;
+    private String surname;
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private  int age;
+
+@Override
+public String toString() {
+    return name + " " + surname + " " + age;
 }
+
+    @Override
+    public int compareTo(Person o) {
+        if (getSurname().length() < o.surname.length()) {
+            return 1;
+        } else if (getSurname().length() > o.surname.length()) {
+            return -1;
+        } else if (getAge() < o.age){
+                return 1;
+        }else if (getAge() > o.age){
+            return -1;
+        }
+        return 0;
+    }
+    }
+
